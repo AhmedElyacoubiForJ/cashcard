19d79b89b318dbad64660ddd454cc7d0840c6b1d
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-11-25 02:58:53 +0100

    cashcard project bootstrap

---
+++ src/test/java/edu/yacoubi/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/edu/yacoubi/cashcard/CashCardJsonTest.java
@@ -0,0 +1,11 @@
+package edu.yacoubi.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+    @Test
+    void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}
